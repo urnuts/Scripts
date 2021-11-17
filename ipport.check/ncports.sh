dff896fe1c2e0e8a9e158384ca00678d5f740537
urnuts <51977940+urnuts@users.noreply.github.com>
2021-11-17 10:54:53 +0800

    Update ncports.sh

--- ipport.check/ncports.sh
+++ ipport.check/ncports.sh
@@ -2,7 +2,7 @@
     #!/bin/bash  
     #检测服务器端口是否开放，成功会返回0值显示ok，失败会返回1值显示fail  
      
-    cat /root/ip.txt | while read line  
+    cat /root/ip-ports.txt | while read line  
     do  
       nc -w 10 -z $line > /dev/null 2>&1  
       if [ $? -eq 0 ]  
