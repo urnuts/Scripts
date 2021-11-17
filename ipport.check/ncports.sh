2311da7d30b94553d2b62a82fd6abd229acbd604
urnuts <51977940+urnuts@users.noreply.github.com>
2021-11-17 10:42:46 +0800

    Add files via upload

---
+++ ipport.check/ncports.sh
@@ -0,0 +1,14 @@
+
+    #!/bin/bash  
+    #检测服务器端口是否开放，成功会返回0值显示ok，失败会返回1值显示fail  
+     
+    cat /root/ip.txt | while read line  
+    do  
+      nc -w 10 -z $line > /dev/null 2>&1  
+      if [ $? -eq 0 ]  
+      then  
+        echo $line:ok  
+      else  
+        echo $line:fail  
+      fi   
+    done 
